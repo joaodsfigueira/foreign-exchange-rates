1c45b5f6b5418d1bfbabee7642b9b635b81f7445
joaodsfigueira <joaodsfigueira@gmail.com>
2025-02-26 05:19:16 +0000

    Added api key validation (with a middleware)

---
+++ src/ForeignExchangeRates.WebAPI/Middlewares/ApiKeyMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Net.Http.Headers;
+using System.Text;
+
+namespace ForeignExchangeRates.WebAPI.Middlewares;
+
+public class ApiKeyMiddleware
+{
+	private readonly RequestDelegate _next;
+	private readonly string _apiKey;
+
+	public ApiKeyMiddleware(RequestDelegate next, IConfiguration configuration)
+	{
+		_next = next;
+		_apiKey = configuration["ApiKey"] ?? throw new InvalidOperationException("ApiKey configuration is invalid.");
+	}
+
+	public async Task InvokeAsync(HttpContext context)
+	{
+		if (!string.Equals(context.Request.Headers["ApiKey"].FirstOrDefault(), _apiKey))
+		{
+			context.Response.StatusCode = 403;
+			await context.Response.WriteAsync("Invalid api key. Please include a valid key on a ApiKey http header.", Encoding.UTF8);
+		}
+		else
+		{
+			await _next(context);
+		}
+	}
+}
